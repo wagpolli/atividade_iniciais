84a78711d90615f2554a7bd06238f6ea94a41dab
Wagner Polli <wagner_polli@estudante.sesisenai.org.br>
2024-06-20 16:58:48 -0300

    exercicio_01

---
+++ lista_01/exercicio_01/index.js
@@ -0,0 +1,10 @@
+
+let botao = document.getElementById("botao")
+
+botao.addEventListener("click", function() {
+   let nome = document.getElementById("nome").value
+
+    document.getElementById("saudacao").innerText = "Olá " + nome + ", este é meu site"
+
+
+})
